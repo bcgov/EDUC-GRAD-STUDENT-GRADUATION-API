a095dd3a82ecfb84bc1d41fe8d948bb6632e0d36
s.karekkattumanasree <s.karekkattumanasreekumaran@cgi.com>
2022-02-07 12:46:01 -0800

    Transcript Message Flyway change

--- api/src/main/resources/db/migration/1.0/V1.0.27__DML-UPDATE_DATA-TRANSCRIPT_MESSAGE.sql
+++ api/src/main/resources/db/migration/1.0/V1.0.27__DML-UPDATE_DATA-TRANSCRIPT_MESSAGE.sql
@@ -1,2 +1,11 @@
 UPDATE TRANSCRIPT_MESSAGE SET GRAD_PROJECTED_MESSAGE = 'Upon completion of this student''s School Completion Certificate Program, a certificate will be issued from the Ministry of Education for meeting their educational program goals other than graduation, as determined by the board of education.' WHERE TRANSCRIPT_MESSAGE_ID = 'C6F6F2FEC76318FCE0539AE9228E9EFD';
 UPDATE TRANSCRIPT_MESSAGE SET GRAD_PROJECTED_MESSAGE = 'Upon completion of this student''s School Completion Certificate Program, a certificate will be issued from the Ministry of Education for meeting their educational program goals other than graduation, as determined by the board of education.' WHERE TRANSCRIPT_MESSAGE_ID = 'C6F6F2FEC76418FCE0539AE9228E9EFD';
+UPDATE TRANSCRIPT_MESSAGE SET PROGRAM_CADRE = 'Student has successfully completed the %s Program' WHERE TRANSCRIPT_MESSAGE_ID='C6F6F2FEC76118FCE0539AE9228E9EFD';
+UPDATE TRANSCRIPT_MESSAGE SET PROGRAM_CADRE = 'Student is participating in the %s Program' WHERE TRANSCRIPT_MESSAGE_ID='C6F6F2FEC76218FCE0539AE9228E9EFD';
+UPDATE TRANSCRIPT_MESSAGE SET PROGRAM_CADRE = 'Student is participating in the %s Program' WHERE TRANSCRIPT_MESSAGE_ID='D32635E887B47239E0539AE9228E76BE';
+UPDATE TRANSCRIPT_MESSAGE SET PROGRAM_CADRE = 'Student is participating in the %s Program' WHERE TRANSCRIPT_MESSAGE_ID='D32635E887B67239E0539AE9228E76BE';
+UPDATE TRANSCRIPT_MESSAGE SET PROGRAM_CADRE = 'Student is participating in the %s Program' WHERE TRANSCRIPT_MESSAGE_ID='C6F6F2FEC76018FCE0539AE9228E9EFD';
+UPDATE TRANSCRIPT_MESSAGE SET PROGRAM_CADRE = 'Student is participating in the %s Program' WHERE TRANSCRIPT_MESSAGE_ID='CA43F7ED2166FC84E0539AE9228EE4EE';
+UPDATE TRANSCRIPT_MESSAGE SET PROGRAM_CADRE = 'Student is participating in the %s Program' WHERE TRANSCRIPT_MESSAGE_ID='CA43F7ED2168FC84E0539AE9228EE4EE';
+UPDATE TRANSCRIPT_MESSAGE SET PROGRAM_CADRE = 'Student is participating in the %s Program' WHERE TRANSCRIPT_MESSAGE_ID='CA43F7ED216AFC84E0539AE9228EE4EE';
+
