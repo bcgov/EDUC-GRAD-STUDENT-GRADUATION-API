6a90d01b99c8708531344d2c1c4af69dc649d873
Jinil Sung <infstar@gmail.com>
2022-04-26 15:49:07 -0700

    Splunk logging feature is newly added.
    Splunk logging feature is newly added.

--- api/src/main/java/ca/bc/gov/educ/api/studentgraduation/config/RequestInterceptor.java
+++ api/src/main/java/ca/bc/gov/educ/api/studentgraduation/config/RequestInterceptor.java
@@ -3,11 +3,17 @@ package ca.bc.gov.educ.api.studentgraduation.config;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ca.bc.gov.educ.api.studentgraduation.util.EducGradStudentGraduationApiConstants;
+import ca.bc.gov.educ.api.studentgraduation.util.LogHelper;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import ca.bc.gov.educ.api.studentgraduation.util.GradValidation;
+
+import java.time.Instant;
 
 @Component
 public class RequestInterceptor extends HandlerInterceptorAdapter {
@@ -15,9 +21,34 @@ public class RequestInterceptor extends HandlerInterceptorAdapter {
 	@Autowired
 	GradValidation validation;
 
+	@Autowired
+	EducGradStudentGraduationApiConstants constants;
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		// for async this is called twice so need a check to avoid setting twice.
+		if (request.getAttribute("startTime") == null) {
+			final long startTime = Instant.now().toEpochMilli();
+			request.setAttribute("startTime", startTime);
+		}
 		validation.clear();
 		return true;
+	}
+
+	/**
+	 * After completion.
+	 *
+	 * @param request  the request
+	 * @param response the response
+	 * @param handler  the handler
+	 * @param ex       the ex
+	 */
+	@Override
+	public void afterCompletion(@NonNull final HttpServletRequest request, final HttpServletResponse response, @NonNull final Object handler, final Exception ex) {
+		LogHelper.logServerHttpReqResponseDetails(request, response, constants.isSplunkLogHelperEnabled());
+		val correlationID = request.getHeader(EducGradStudentGraduationApiConstants.CORRELATION_ID);
+		if (correlationID != null) {
+			response.setHeader(EducGradStudentGraduationApiConstants.CORRELATION_ID, request.getHeader(EducGradStudentGraduationApiConstants.CORRELATION_ID));
+		}
 	}
 }
