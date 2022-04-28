1b1b7c97338b571c22e4d5f1acc642e802871474
Jinil Sung <infstar@gmail.com>
2022-04-28 16:21:59 -0700

    Update RequestInterceptor.java
    Bug fix on clearing Thread Local variables.

--- api/src/main/java/ca/bc/gov/educ/api/studentgraduation/config/RequestInterceptor.java
+++ api/src/main/java/ca/bc/gov/educ/api/studentgraduation/config/RequestInterceptor.java
@@ -66,9 +66,9 @@ public class RequestInterceptor extends HandlerInterceptorAdapter {
 		val correlationID = request.getHeader(EducGradStudentGraduationApiConstants.CORRELATION_ID);
 		if (correlationID != null) {
 			response.setHeader(EducGradStudentGraduationApiConstants.CORRELATION_ID, request.getHeader(EducGradStudentGraduationApiConstants.CORRELATION_ID));
-			// reset
-			ThreadLocalStateUtil.setCorrelationID(null);
-			ThreadLocalStateUtil.setCurrentUser(null);
 		}
+		// reset
+		ThreadLocalStateUtil.setCorrelationID(null);
+		ThreadLocalStateUtil.setCurrentUser(null);
 	}
 }
