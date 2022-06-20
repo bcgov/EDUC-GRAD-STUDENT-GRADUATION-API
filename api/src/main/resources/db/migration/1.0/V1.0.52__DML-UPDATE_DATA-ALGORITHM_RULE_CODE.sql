70782a3040f07664a9f81e78b8acbc4783c0bbc3
alexryba <alex.rybakov@gov.bc.ca>
2022-06-20 11:39:35 -0600

    GRAD2-1320: Add new rule to Algorithm Rule Code and Program Algorithm Rule tables

--- api/src/main/resources/db/migration/1.0/V1.0.52__DML-UPDATE_DATA-ALGORITHM_RULE_CODE.sql
+++ api/src/main/resources/db/migration/1.0/V1.0.52__DML-UPDATE_DATA-ALGORITHM_RULE_CODE.sql
@@ -1,5 +1,35 @@
-REM INSERTING into ALGORITHM_RULE_CODE
-Insert into ALGORITHM_RULE_CODE (ALGORITHM_RULE_CODE,RULE_IMPL_NAME,LABEL,DESCRIPTION,ACTIVE_RULE,DISPLAY_ORDER,CREATE_USER,CREATE_DATE,UPDATE_USER,UPDATE_DATE) values ('EXCL_ASSMT_AFT_CUTOFF_RULE','ExcludeAssmtAfterAssessmentCutoffRule',' Exclude assmts after Assessment Cutoff Date rule','Assessments taken after the Assessment Cutoff Date will not be used to determine graduation or projected graduation and will not be reported','Y',490,'API_STUDENT_GRADUATION',to_date('22-06-16','RR-MM-DD'),'API_STUDENT_GRADUATION',to_date('22-06-16','RR-MM-DD'));
+begin
+    for cur in (select fk.owner, fk.constraint_name , fk.table_name
+                from all_constraints fk, all_constraints pk
+                where fk.CONSTRAINT_TYPE = 'R' and
+                        pk.owner = 'API_STUDENT_GRADUATION' and
+                        fk.r_owner = pk.owner and
+                        fk.R_CONSTRAINT_NAME = pk.CONSTRAINT_NAME and
+                        fk.TABLE_NAME = 'PROGRAM_ALGORITHM_RULE') loop
+            execute immediate 'ALTER TABLE "'||cur.owner||'"."'||cur.table_name||'" MODIFY CONSTRAINT "'||cur.constraint_name||'" DISABLE';
+        end loop;
+end;
+/
+
+UPDATE ALGORITHM_RULE_CODE SET LABEL = 'Registration Duplicate Course Rule', RULE_IMPL_NAME = 'RegistrationsDuplicateCrseRule', ALGORITHM_RULE_CODE = 'REGTN_DUPLICATE_CRSE_RULE' WHERE ALGORITHM_RULE_CODE = 'REGTN_DUPLICATE_RULE';
+/
+UPDATE PROGRAM_ALGORITHM_RULE SET ALGORITHM_RULE_CODE = 'REGTN_DUPLICATE_CRSE_RULE' WHERE ALGORITHM_RULE_CODE = 'REGTN_DUPLICATE_RULE';
+/
+
+begin
+    for cur in (select fk.owner, fk.constraint_name , fk.table_name
+                from all_constraints fk, all_constraints pk
+                where fk.CONSTRAINT_TYPE = 'R' and
+                        pk.owner = 'API_STUDENT_GRADUATION' and
+                        fk.r_owner = pk.owner and
+                        fk.R_CONSTRAINT_NAME = pk.CONSTRAINT_NAME and
+                        fk.TABLE_NAME = 'PROGRAM_ALGORITHM_RULE') loop
+            execute immediate 'ALTER TABLE "'||cur.owner||'"."'||cur.table_name||'" MODIFY CONSTRAINT "'||cur.constraint_name||'" ENABLE';
+        end loop;
+end;
+/
+
+Insert into ALGORITHM_RULE_CODE (ALGORITHM_RULE_CODE,RULE_IMPL_NAME,LABEL,DESCRIPTION,ACTIVE_RULE,DISPLAY_ORDER,CREATE_USER,CREATE_DATE,UPDATE_USER,UPDATE_DATE) values ('REGTN_DUPLICATE_ASSMT_RULE','RegistrationsDuplicateAssmtRule','Registration Duplicate Assessment Rule','Checks if there are duplicate in progress assessments','Y',490,'API_STUDENT_GRADUATION',to_date('22-06-16','RR-MM-DD'),'API_STUDENT_GRADUATION',to_date('22-06-16','RR-MM-DD'));
 /
 
 DECLARE
@@ -7,7 +37,7 @@ DECLARE
 BEGIN
     SELECT MAX(SORT_ORDER) + 10 INTO v_sort FROM PROGRAM_ALGORITHM_RULE;
     FOR x IN (SELECT DISTINCT GRADUATION_PROGRAM_CODE FROM PROGRAM_ALGORITHM_RULE) LOOP
-        Insert into PROGRAM_ALGORITHM_RULE (PROGRAM_ALGO_RULE_ID,GRADUATION_PROGRAM_CODE,ALGORITHM_RULE_CODE,SORT_ORDER,CREATE_USER,CREATE_DATE,UPDATE_USER,UPDATE_DATE) values (SYS_GUID(),x.GRADUATION_PROGRAM_CODE,'EXCL_ASSMT_AFT_CUTOFF_RULE',v_sort,'API_STUDENT_GRADUATION',to_date('22-06-16','RR-MM-DD'),'API_STUDENT_GRADUATION',to_date('22-06-16','RR-MM-DD'));
+        Insert into PROGRAM_ALGORITHM_RULE (PROGRAM_ALGO_RULE_ID,GRADUATION_PROGRAM_CODE,ALGORITHM_RULE_CODE,SORT_ORDER,CREATE_USER,CREATE_DATE,UPDATE_USER,UPDATE_DATE) values (SYS_GUID(),x.GRADUATION_PROGRAM_CODE,'REGTN_DUPLICATE_ASSMT_RULE',v_sort,'API_STUDENT_GRADUATION',to_date('22-06-16','RR-MM-DD'),'API_STUDENT_GRADUATION',to_date('22-06-16','RR-MM-DD'));
     END LOOP;
 END;
 /
